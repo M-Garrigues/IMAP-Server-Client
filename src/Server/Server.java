dc258a539403aa90ca5b924686b6f0018631ee9b
MONNERET THOMAS p1506193 <thomas.monneret@etu.univ-lyon1.fr>
2019-03-18 11:45:44 +0100

    First server commit

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,13 +1,26 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server{
     private static final int PORT = 110;
 
-    public static void main(String[] args) {
-        List<ServerThread> serverThreads = new List<>();
-        Socket socket = new Socket(PORT);
-        while(true){
-            System.out.println("test");
-            //if(serverThreads.)
+    public static void main(String[] args) throws IOException {
+        List<ServerThread> serverThreads = new ArrayList<>();
+        ServerSocket serverSocket = new ServerSocket(PORT);
 
+        while(true){
+            //Si le dernier thread créé n'est pas dans l'état Ready, on en crée un nouveau dans cet état
+            //(pour être en mesure d'accepter une nouvelle connexion entrante)
+            if(serverThreads.isEmpty() || serverThreads.get(serverThreads.size()-1).getServerState() != StateEnum.READY){
+                ServerThread serverThread = new ServerThread(serverSocket);
+                serverThread.run();
+                serverThreads.add(serverThread);
+            }
         }
 
     }
