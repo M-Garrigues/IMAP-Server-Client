94f354792bfcb7736484e0bee5e7384715e58cc1
MONNERET THOMAS p1506193 <thomas.monneret@etu.univ-lyon1.fr>
2019-03-18 11:35:49 +0100

    First server commit

---
+++ src/Server/Server.java
@@ -0,0 +1,14 @@
+public class Server{
+    private static final int PORT = 110;
+
+    public static void main(String[] args) {
+        List<ServerThread> serverThreads = new List<>();
+        Socket socket = new Socket(PORT);
+        while(true){
+            System.out.println("test");
+            //if(serverThreads.)
+
+        }
+
+    }
+}
