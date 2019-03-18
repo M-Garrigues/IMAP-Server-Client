960814d8d1b376b2d7f18e89250272479f03bbf9
MONNERET THOMAS p1506193 <thomas.monneret@etu.univ-lyon1.fr>
2019-03-18 11:45:44 +0100

    First server commit

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -1,7 +1,83 @@
-public class ServerThread extends Thread{
-    private String state;
+package Server;
 
-    public String getState(){
-        return state;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerThread extends Thread{
+    //private static final List<String> users = new ArrayList<String>.
+
+    private StateEnum serverState;
+    private ServerSocket serverSocket;
+    private Socket serverThreadSocket;
+    private InputStreamReader inputStreamReader;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private String currentUser;
+
+    public ServerThread(ServerSocket serverSocket) throws IOException {
+        this.serverState = StateEnum.STOPPED;
+        this.serverSocket = serverSocket;
+    }
+
+    @Override
+    public void run(){
+        serverState = StateEnum.READY;
+
+        //Attente d'une connexion
+        try {
+            serverThreadSocket = serverSocket.accept();
+
+            //Initialisation des canaux d'entrée et de sortie
+            inputStreamReader = new InputStreamReader(serverThreadSocket.getInputStream());
+            out = new PrintWriter(serverThreadSocket.getOutputStream(), true);
+            in = new BufferedReader(inputStreamReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Passage dans l'état Authorization
+        serverState = StateEnum.AUTHORIZATION;
+        out.println("+OK POP3 server ready");
+
+        while(true){
+            switch(serverState){
+                case AUTHORIZATION:
+                    handleAuthorizationState();
+                    break;
+            }
+        }
+
+    }
+
+    private void handleAuthorizationState(){
+        String input;
+            try {
+                while((input = in.readLine()) != null){
+                    String[] params = input.split(" ", 2);
+                    if(params[0].equals("USER")){
+                        if(!params[1].equals("utilisateur1") && !params[1].equals("utilisateur2")){
+                            out.println("-ERR unknown user : " + params[1]);
+                        }
+                        else{
+                            currentUser = params[1];
+
+                            out.println("+OK");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public StateEnum getServerState(){
+        return serverState;
     }
 }
